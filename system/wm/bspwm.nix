525e52f9c0beacec31227562f671b77321c0d36c
David Vogel <dvogel@fastmail.com>
2021-01-11 12:38:42 -0500

    Added bspwm

---
+++ system/wm/bspwm.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services = {
+    gnome3.gnome-keyring.enable = true;
+    upower.enable = true;
+
+    dbus = {
+      enable = true;
+      packages = [ pkgs.gnome3.dconf ];
+    };
+
+    xserver = {
+      enable = true;
+
+      layout = "us";
+
+      libinput = {
+        enable = true;
+        disableWhileTyping = true;
+      };
+
+      videoDrivers = [ "modesetting" ];
+      useGlamor = true;
+
+      serverLayoutSection = ''
+        Option "StandbyTime" "0"
+        Option "SuspendTime" "0"
+        Option "OffTime"     "0"
+      '';
+
+      displayManager = {
+        defaultSession = "none+bspwm";
+      };
+
+      windowManager.bspwm = {
+        enable = true;
+      };
+
+      xkbOptions = "ctrl:nocaps";
+    };
+  };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  systemd.services.upower.enable = true;
+}
